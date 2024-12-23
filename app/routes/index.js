fd8df069fa7bb149e24c6cefc922f1449f32b77f
Sam Bordo <sambordo1@gmail.com>
2024-12-23 16:21:46 -0500

    add more scriptures

--- app/routes/index.js
+++ app/routes/index.js
@@ -7,10 +7,10 @@ export default class IndexRoute extends Route {
       { id: 2, text: 'God does not love me' },
       { id: 3, text: 'God does not answer my prayers' },
       { id: 4, text: 'God does not see me' },
-      { id: 4, text: 'Waiting on God is futile' },
-      { id: 5, text: 'It is bad to show weakness and be vulnerable' },
-      { id: 6, text: 'I am alone' },
-      { id: 7, text: 'I cannot overcome sin' },
+      { id: 5, text: 'Waiting on God is futile' },
+      { id: 6, text: 'It is bad to show weakness and be vulnerable' },
+      { id: 7, text: 'I am alone' },
+      { id: 8, text: 'I cannot overcome sin' },
     ];
   }
 }
