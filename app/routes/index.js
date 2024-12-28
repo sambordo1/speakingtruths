f64fea50b98d9da21c511e5b51eed194a18628d1
Sam Bordo <sambordo1@gmail.com>
2024-12-28 17:43:02 -0500

    add waiting scriptures

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,11 +3,11 @@ import Route from '@ember/routing/route';
 export default class IndexRoute extends Route {
   model() {
     return [
-      { id: 1, text: 'There is No Hope' },
-      { id: 2, text: 'God does not love me' },
-      { id: 3, text: 'God does not answer my prayers' },
-      { id: 4, text: 'God does not see me' },
-      { id: 5, text: 'Waiting on God is futile' },
+      { id: 1, text: 'Waiting on God is futile' },
+      { id: 2, text: 'There is No Hope' },
+      { id: 3, text: 'God does not love me' },
+      { id: 4, text: 'God does not answer my prayers' },
+      { id: 5, text: 'God does not see me' },
       { id: 6, text: 'It is bad to show weakness and be vulnerable' },
       { id: 7, text: 'I am alone' },
       { id: 8, text: 'I cannot overcome sin' },
